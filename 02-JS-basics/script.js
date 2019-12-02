de4e775a63f64d94aa8d12f0c77526dc9b80d8dd
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-12-02 16:01:16 +0300

    Complete lessons 23-26

--- 02-JS-basics/script.js
+++ 02-JS-basics/script.js
@@ -301,7 +301,7 @@ console.log(whatDoYouDo('retired', 'Mark'));*/
  * Arrays
  * */
 
-// Initialize new array
+/*// Initialize new array
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
 
@@ -332,4 +332,54 @@ console.log(john);
 console.log(john.indexOf(23));
 
 var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
-console.log({ isDesigner });
+console.log({ isDesigner });*/
+
+/*
+ * Objects and properties
+ * */
+
+/*// Object literal
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+};
+console.log(john);
+
+console.log(john.firstName);
+console.log(john['lastName']);
+
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// new Object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);*/
+
+/*
+ * Objects and methods
+ * */
+
+// var john = {
+//   firstName: 'John',
+//   lastName: 'Smith',
+//   birthYear: 1992,
+//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
+//   job: 'teacher',
+//   isMarried: false,
+//   calcAge: function() {
+//     this.age = 2018 - this.birthYear;
+//   },
+// };
+// john.calcAge();
+// console.log(john);
